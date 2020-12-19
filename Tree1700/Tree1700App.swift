372a42c9aad995bbfaae6ba2a3798a1f23135cb6
Ivan Dimitrov <biobeehive@abv.bg>
2020-12-19 09:56:08 +0200

    Initial Commit

---
+++ Tree1700/Tree1700App.swift
@@ -0,0 +1,17 @@
+//
+//  Tree1700App.swift
+//  Tree1700
+//
+//  Created by Ivan Dimitrov on 19.12.20.
+//
+
+import SwiftUI
+
+@main
+struct Tree1700App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
